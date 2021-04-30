34835363b11d885d78f6191bdc7c81d3f65a0454
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-04-30 23:28:57 +0530

    adding style component to searchMovie

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,15 +1,20 @@
 import React from 'react';
 
 export default function SearchMovie() {
-
-    return (
-        <form className="form">
-            <label htmlFor="query" className="Label">
-                Movie Name
-                <input className="input" type="text" name="query" placeholder="i.e. Tenet" />
-            </label>
-            <button type="submit" value="Submit" className="button">Search</button>
-        </form> 
-    )
-
+  return (
+    <form className='form'>
+      <label htmlFor='query' className='label'>
+        Movie Name
+      </label>
+      <input
+        className='input'
+        type='text'
+        name='query'
+        placeholder='i.e. Tenet'
+      />
+      <button type='submit' value='Submit' className='button'>
+        Search
+      </button>
+    </form>
+  );
 }
