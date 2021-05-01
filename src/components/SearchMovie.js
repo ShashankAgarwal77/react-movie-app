6569873b16ba900cfcd65137707d4b1f5ba70aa6
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-05-01 12:34:08 +0530

    manage state with react useState Hook

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function SearchMovie() {
+  //states - input query, movies
+  const [query, setQuery] = useState('');
+
+  //create the state for movies, and update that state appropriate
+  const [movies, setMovies] = useState([]);
+
   const searchMovie = async (e) => {
     e.preventDefault();
-    console.log('submitting');
-
-    const query = 'Tenet';
 
     const url = `https://api.themoviedb.org/3/search/movie?api_key=8f882c9bcf06cf300c9055ec482c60e2&language=en-US&query=${query}&page=1&include_adult=false`;
 
     try {
       const res = await fetch(url);
       const data = await res.json();
-      console.log(data);
+      setMovies(data.results);
     } catch (err) {
       console.log(err);
     }
@@ -28,6 +31,8 @@ export default function SearchMovie() {
         type='text'
         name='query'
         placeholder='i.e. Tenet'
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
       />
       <button type='submit' value='Submit' className='button'>
         Search
