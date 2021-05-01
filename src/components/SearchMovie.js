bfadae9d72493023684231a404e740a1f2bc3378
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-05-01 13:07:42 +0530

    created the movie card

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -22,21 +22,46 @@ export default function SearchMovie() {
   };
 
   return (
-    <form className='form' onSubmit={searchMovie}>
-      <label htmlFor='query' className='label'>
-        Movie Name
-      </label>
-      <input
-        className='input'
-        type='text'
-        name='query'
-        placeholder='i.e. Tenet'
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
-      <button type='submit' value='Submit' className='button'>
-        Search
-      </button>
-    </form>
+    <>
+      <form className='form' onSubmit={searchMovie}>
+        <label htmlFor='query' className='label'>
+          Movie Name
+        </label>
+        <input
+          className='input'
+          type='text'
+          name='query'
+          placeholder='i.e. Tenet'
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type='submit' value='Submit' className='button'>
+          Search
+        </button>
+      </form>
+      <div className='card-list'>
+        {movies
+          .filter((movie) => movie.poster_path)
+          .map((movie) => (
+            <div className='card'>
+              <img
+                className='card--image'
+                src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                alt={movie.title + 'poster'}
+              />
+              <div className='card--content'>
+                <h3 className='card--title'>{movie.title}</h3>
+                <p className='card-paragraph'>
+                  <small> RELEASE DATE: {movie.release_date}</small>
+                </p>
+                <p className='card-paragraph'>
+                  <small> RATING: {movie.vote_average}</small>
+                </p>
+                <p className="card-desc">{movie.overview}</p>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
   );
 }
