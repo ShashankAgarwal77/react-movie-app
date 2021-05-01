3ca09283be6b5b2e2e95d00886e4e3c7eaa7248b
Shashank Agarwal <shishya.agarwal@gmail.com>
2021-05-01 09:27:28 +0530

    created the search movies function using async/await and fetching API from themoviesAPI

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
 export default function SearchMovie() {
+  const searchMovie = async (e) => {
+    e.preventDefault();
+    console.log('submitting');
+
+    const query = 'Tenet';
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=8f882c9bcf06cf300c9055ec482c60e2&language=en-US&query=${query}&page=1&include_adult=false`;
+
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
-    <form className='form'>
+    <form className='form' onSubmit={searchMovie}>
       <label htmlFor='query' className='label'>
         Movie Name
       </label>
